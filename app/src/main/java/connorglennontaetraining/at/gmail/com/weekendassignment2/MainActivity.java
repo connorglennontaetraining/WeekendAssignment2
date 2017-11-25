a7a1f34fe62e51d9931a3ed77f1a9589eb7d7dc1
Fat-Llama <connorglennon@outlook.com>
2017-11-25 13:08:53 +0000

    Initial commit, base setup complete. Removed auto created TextViews. Reorganised MainActivity. Added fragment container.

---
+++ app/src/main/java/connorglennontaetraining/at/gmail/com/weekendassignment2/MainActivity.java
@@ -0,0 +1,43 @@
+package connorglennontaetraining.at.gmail.com.weekendassignment2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        Initialise the bottom navigation
+         */
+        initBottomNavigation();
+    }
+
+    private void initBottomNavigation()
+    {
+        mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }};
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
